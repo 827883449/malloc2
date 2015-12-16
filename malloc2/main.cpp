9ad2b7175d0e71551a266ccd2c1d2a27b1a8b1db
827883449 <827883449@qq.com>
2015-12-16 19:20:45 +0800

    malloc
    malloc 排序 第二次提交主
    函数中定义了a[N]数组以及i，j，temp等变量，用for语句的循环输入一组数据并保存，通过冒泡排序把输入的数据从大到小进行排序，输出数据。

--- malloc2/main.cpp
+++ malloc2/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#define N 5
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int *a;
+    int i,j;
+    int temp;
+    a=new int[N];
+    if(a==0)
+        exit(0);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<N;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    delete a;
     return 0;
 }
